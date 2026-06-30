ac4d0fa112ac63958193ba8e0875f83f5f23833b
Sakshi Dubey <dubeysakshi0117@gmail.com>
2026-06-30 09:09:06 +0530

    Add snake, water, gun game logic
    Implemented a simple game where the user can choose between snake, water, or gun, and the computer randomly selects an option. Added win/lose conditions based on user input.

--- index.py
+++ index.py
@@ -1,1 +1,34 @@
 print = ("Hello world")
+
+
+'''
+1 for snake 
+-1 for water
+o for gun
+'''
+
+computer =-1
+youstr = input("Enter your choice:")
+youDict ={"s":1,"w":-1, "g":0}
+you = youDict[youstr]
+
+if (computer ==-1 and you ==1):
+    print("you win!")
+
+elif(computer==-1 and you ==0):
+    print("you Lose!")
+
+if (computer ==1 and you ==-1):
+    print("you Lose!")
+
+elif(computer==1 and you ==0):
+    print("you win!") 
+
+if (computer ==0 and you ==-1):
+    print("you win!")
+
+elif(computer==0 and you ==1):
+    print("you Lose!")
+    
+else:
+    print("something went wrong!")
